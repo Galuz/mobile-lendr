e8eeeedb91c5bacc283a34df544e46656ccc1334
German Gonzalez <german@vetta.io>
2021-01-26 21:01:33 -0600

    main - se maquetó el navbar

---
+++ layouts/navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="layout-nav">
+      <b-navbar type="dark" variant="dark">
+            <b-navbar-brand href="#">
+                <img src="@/assets/images/robot.svg" alt="Logo">
+                <span>Integra <b>Software</b></span>
+            </b-navbar-brand>
+            <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+            <b-collapse v-model="isCollapseOpen" id="nav-text-collapse" is-nav>
+                <b-navbar-nav class="nav-item ml-auto">
+                    <li>
+                        Miguel Mencía
+                    </li>
+                </b-navbar-nav>
+            </b-collapse>
+      </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
